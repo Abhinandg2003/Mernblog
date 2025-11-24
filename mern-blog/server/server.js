9848bfa253385c76c8113e9cad9ceacefbff2e3e
Abhinand G <abhinandg2003@gmail.com>
2025-11-24 18:22:27 +0530

    Update server.js

--- mern-blog/server/server.js
+++ mern-blog/server/server.js
@@ -26,9 +26,9 @@ app.use('/posts', postRoutes);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
-  );
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 }
 
 // MongoDB + Server start
